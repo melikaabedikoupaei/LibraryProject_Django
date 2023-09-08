a5930ff693fea51bd622716c6a526b20b5473b44
unknown <melikaa@gmail.com>
2023-09-08 09:18:59 +0200

    edit view with TemplateView

--- catalog/views.py
+++ catalog/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
-
-def index(request):
-    """
-    View function for home page of site.
-    """
-    # Generate counts of some of the main objects
-    num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
-    # Available books (status = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # The 'all()' is implied by default.
+from django.views.generic.base import TemplateView
+##for comment I use ctrl+k+c
+# def index(request): 
+#     """
+#     View function for home page of site.
+#     """
+#     # Generate counts of some of the main objects
+#     num_books=Book.objects.all().count()
+#     num_instances=BookInstance.objects.all().count()
+#     # Available books (status = 'a')
+#     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+#     num_authors=Author.objects.count()  # The 'all()' is implied by default.
     
-    # Render the HTML template index.html with the data in the context variable
-    return render(
-        request,
-        'catalog/index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
-    )
+#     # Render the HTML template index.html with the data in the context variable
+#     return render(
+#         request,
+#         'catalog/index.html',
+#         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+#     )
 
 
+
+class Index(TemplateView):
+
+    template_name = "catalog/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['num_books'] =Book.objects.all().count()
+        context['num_instances'] =BookInstance.objects.all().count()
+        context['num_instances_available'] =BookInstance.objects.filter(status__exact='a').count()
+        context['num_authors'] =Author.objects.count() 
+        return context
